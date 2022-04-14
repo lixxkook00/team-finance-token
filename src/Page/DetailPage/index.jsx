dc40a1af77e60671bdb443a935a9fa5812299a8c
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-04-14 09:59:57 +0700

    header and first card in detail page

--- src/Page/DetailPage/index.jsx
+++ src/Page/DetailPage/index.jsx
@@ -9,16 +9,19 @@ function DetailPage(props) {
            <div className="detail-container">
                 <div className="container">
                     <div className="row">
+                        {/* direction */}
                         <div className="col-xl-12">
                             <Link to='/' className="detail-direction">
                                 <i class="fa-solid fa-arrow-left"></i>
                                 <p>Overview</p>
                             </Link>
                         </div>
+
+                        {/* infor */}
                         <div className="col-xl-12">
                             <div className="detail-row">
                                 <div className="detail-infor">
-                                    <img src="/img/token-logo.jpg" alt="" />
+                                    <img className="detail-infor-avt" src="/img/token-logo.jpg" alt="" />
                                     <div>
                                         <div className="detail-infor-title">
                                             Everdome
@@ -38,14 +41,121 @@ function DetailPage(props) {
                                         REQUEST HACKEN AUDIT
                                     </div>
                                     <div className="btn-second">
+                                        <img src="/img/icon (4).png" alt="" />
                                         LOCK DOME
                                     </div>
                                     <div className="btn-second">
+                                        <img src="/img/icon (5).png" alt="" />
                                         LOCK LIQUIDITY
                                     </div>
                                 </div>
                             </div>
                         </div>
+
+                        {/* content */}
+
+                        <div className="col-xl-6 mt-30">
+                                <div className="detail-card">
+                                    <div className="detail-card-first">
+                                        <div className="detail-card-title">
+                                            Everdome Lockup Overview
+                                        </div>
+                                        <div className="detail-card-decs">
+                                            View liquidity and DOME lockup information
+                                        </div>
+                                    </div>
+                                    <div className="detail-li detail-overview-li">
+                                        <div className="detail-overview-infor">
+                                            <img src="/img/icon (5).png" alt="" />
+                                            <div>
+                                                <div className="detail-overview-infor_name">
+                                                    Liquidity Locked
+                                                </div>
+                                                <div className="detail-overview-infor_decs">
+                                                    Pancake LPs
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="detail-overview-value">
+                                            0% Liquidity
+                                        </div>
+                                    </div>
+
+                                    <div className="detail-li detail-overview-li">
+                                        <div className="detail-overview-infor">
+                                            <img src="/img/icon (4).png" alt="" />
+                                            <div>
+                                                <div className="detail-overview-infor_name">
+                                                    DOME Locked
+                                                </div>
+                                                <div className="detail-overview-infor_decs">
+                                                    Binance Smart Chain
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="detail-overview-value">
+                                            600,000,000.00 DOME (0.600%)
+                                        </div>
+                                    </div>
+
+                                    <div className="detail-li detail-overview-li">
+                                        <div className="detail-overview-infor">
+                                            <img src="/img/icon (4).png" alt="" />
+                                            <div>
+                                                <div className="detail-overview-infor_name">
+                                                    Circulating Supply
+                                                </div>
+                                                <div className="detail-overview-infor_decs">
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="detail-overview-value">
+                                           99,399,999,999.99 DOME
+                                        </div>
+                                    </div>
+
+                                    <div className="detail-li detail-overview-li">
+                                        <div className="detail-overview-infor">
+                                            <img src="/img/icon (7).png" alt="" />
+                                            <div>
+                                                <div className="detail-overview-infor_name">
+                                                    Total Supply
+                                                </div>
+                                                <div className="detail-overview-infor_decs">
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="detail-overview-value">
+                                           99,999,999,999.99 DOME
+                                        </div>
+                                    </div>
+
+                                    <div className="detail-li detail-overview-li">
+                                        <div className="detail-overview-infor">
+                                            <img src="/img/icon (8).png" alt="" />
+                                            <div>
+                                                <div className="detail-overview-infor_name">
+                                                    CoinGecko Ranking
+                                                </div>
+                                                <div className="detail-overview-infor_decs">
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="detail-overview-value">
+                                           197
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
                     </div>
                 </div>
            </div>
